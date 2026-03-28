fa7c58595d458a4e4f005391d092f5ae9d5fb3fa
Nolan-30 <nolan.dalmeida07@gmail.com>
2026-03-28 20:23:41 +0100

    page d'accueil fini en react

--- snk-wiki/src/App.jsx
+++ snk-wiki/src/App.jsx
@@ -1,16 +1,32 @@
-import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+// import des composants
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 import Characters from "./components/Characters";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
+  }, []);
+
   return (
-    <>
+    <Router>
       <Header />
-      <Characters />
-      <Footer />
-    </>
+      <main style={{ minHeight: "80vh" }}>
+        {" "}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/perso" element={<Characters />} />
+        </Routes>
+      </main>
+      <Footer /> {/* <--- Il doit être ICI, après les Routes */}
+    </Router>
   );
 }
 
